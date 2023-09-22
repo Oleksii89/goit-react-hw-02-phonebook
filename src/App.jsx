4eb8d4004df735889c36c3f6bd6fc61cd7ef5756
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-09-22 15:25:03 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,57 @@
 import { Component } from 'react';
 
-class App extends Component {
+export default class App extends Component {
   state = {
     contacts: [],
     name: '',
   };
 
   handleChange = evt => {
-    console.log(this.state.name);
+    console.log(evt.target.value);
+    console.log(evt.target.name);
 
-    return this.setState({ name: evt.target.value });
+    this.setState({ name: evt.target.value });
   };
   handleSubmit = evt => {
     evt.preventDefault();
     // this.props.onSubmit({ ...this.state });
-    // console.log(this.state);
-    // this.reset();
+    console.log(this.state);
+    this.reset();
   };
 
   render() {
     return (
       <>
-        <h2>Phonebook</h2>
-        <div>
-          <form>
-            <label>
-              Name
-              <input
-                type="text"
-                name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-                value={this.state.name}
-                onChange={this.handleChange}
-                onSubmit={this.handleSubmit}
-              />
-            </label>
-            <button type="submit">Add contact</button>
-          </form>
-        </div>
+        <form onSubmit={this.handleSubmit}>
+          <h2>Phonebook</h2>
+          <label>
+            <span>Name</span>
+
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label>
+          {/* <label>
+            <span>Number</span>
+
+            <input
+              type="text"
+              name="number"
+              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </label> */}
+          <button type="submit">Add contact</button>
+        </form>
         <div>
           <p>Contacts</p>
           <ul>
@@ -52,5 +64,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
