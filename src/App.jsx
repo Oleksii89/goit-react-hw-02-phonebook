35afc65c29bbee7e2d41da31710bb336818cf6bf
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-09-22 13:52:31 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleChange = evt => {
+    console.log(this.state.name);
+
+    return this.setState({ name: evt.target.value });
+  };
+  handleSubmit = evt => {
+    // evt.preventDefault();
+    // this.props.onSubmit({ ...this.state });
+    // console.log(this.state);
+    // this.reset();
+  };
+
+  render() {
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <div>
+          <form>
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                value={this.state.name}
+                onChange={this.handleChange}
+                onSubmit={this.handleSubmit}
+              />
+            </label>
+            <button type="button">Add contact</button>
+          </form>
+        </div>
+        <div>
+          <p>Contacts</p>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
